0a014404808d1715420c2d3c7430d60254ecf490
maju <mjuliafs07@email.com>
2025-02-21 11:25:59 -0300

    Preparação inicial do Projeto -Definição de classe main Configuração path /airports Configuração base H2 para testes Add import.sql

---
+++ src/main/java/dev/maju/br/airports/AirportsApplication.java
@@ -0,0 +1,13 @@
+package dev.maju.br.airports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportsApplication.class, args);
+	}
+
+}
